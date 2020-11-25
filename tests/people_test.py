09caa481bebbfbfeaa7130980dac1d0894d87e87
Gaurav Singh <gaurav.singh@go-jek.com>
2020-11-25 21:16:46 +0530

    Added test to show use of POST method to create a new person and assert if it is present in get requests

--- tests/people_test.py
+++ tests/people_test.py
@@ -1,8 +1,10 @@
 import requests
 from assertpy.assertpy import assert_that
 
+from json import dumps
 from config import BASE_URI
 from utils.print_helpers import pretty_print
+from uuid import uuid4
 
 
 def test_read_all_has_kent():
@@ -13,3 +15,23 @@ def test_read_all_has_kent():
     assert_that(response.status_code).is_equal_to(200)
     first_names = [people['fname'] for people in response_text]
     assert_that(first_names).contains('Kent')
+
+
+def test_new_person_can_be_added():
+    unique_last_name = f'User {str(uuid4())}'
+    payload = dumps({
+        'fname': 'New',
+        'lname': unique_last_name
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    response = requests.post(url=BASE_URI, data=payload, headers=headers)
+    assert_that(response.status_code).is_equal_to(204)
+
+    people = requests.get(BASE_URI).json()
+    is_new_user_created = filter(lambda person: person['lname'] == unique_last_name, people)
+    assert_that(is_new_user_created).is_true()
