2ed2046d486c3abc234e8fb9238349b959a8eed4
Gaurav Singh <gaurav.singh@go-jek.com>
2020-12-28 07:12:52 +0530

    Extracted schema outside the functions and added a separate function to validate it for read one operation

--- tests/schema_test.py
+++ tests/schema_test.py
@@ -5,15 +5,26 @@ from cerberus import Validator
 
 from config import BASE_URI
 
+schema = {
+    "fname": {'type': 'string'},
+    "lname": {'type': 'string'},
+    "person_id": {'type': 'integer'},
+    "timestamp": {'type': 'string'}
+}
+
+
+def test_read_one_operation_has_expected_schema():
+    response = requests.get(f'{BASE_URI}/1')
+    person = json.loads(response.text)
+
+    validator = Validator(schema)
+    is_valid = validator.validate(person)
+
+    if not is_valid:
+        print(validator.errors)
+
 
 def test_read_all_operation_has_expected_schema():
-    schema = {
-        "fname": {'type': 'string'},
-        "lname": {'type': 'string'},
-        "person_id": {'type': 'integer'},
-        "timestamp": {'type': 'string'}
-    }
-
     response = requests.get(BASE_URI)
     persons = json.loads(response.text)
 
