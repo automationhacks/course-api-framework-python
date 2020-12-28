e8c001664485feadf8fe4174bc155f967ce234f6
Gaurav Singh <gaurav.singh@go-jek.com>
2020-12-28 12:37:22 +0530

    Updated schema tests to use assertpy fluent assertions to validate instead of printing on the console.

--- tests/schema_test.py
+++ tests/schema_test.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+from assertpy import assert_that, soft_assertions
 from cerberus import Validator
 
 from config import BASE_URI
@@ -8,7 +9,7 @@ from config import BASE_URI
 schema = {
     "fname": {'type': 'string'},
     "lname": {'type': 'string'},
-    "person_id": {'type': 'integer'},
+    "person_id": {'type': 'number'},
     "timestamp": {'type': 'string'}
 }
 
@@ -17,21 +18,19 @@ def test_read_one_operation_has_expected_schema():
     response = requests.get(f'{BASE_URI}/1')
     person = json.loads(response.text)
 
-    validator = Validator(schema)
+    validator = Validator(schema, require_all=True)
     is_valid = validator.validate(person)
 
-    if not is_valid:
-        print(validator.errors)
+    assert_that(is_valid, description=validator.errors).is_true()
 
 
 def test_read_all_operation_has_expected_schema():
     response = requests.get(BASE_URI)
     persons = json.loads(response.text)
 
-    validator = Validator(schema)
+    validator = Validator(schema, require_all=True)
 
-    for person in persons:
-        is_valid = validator.validate(person)
-
-        if not is_valid:
-            print(validator.errors)
+    with soft_assertions():
+        for person in persons:
+            is_valid = validator.validate(person)
+            assert_that(is_valid, description=validator.errors).is_true()
